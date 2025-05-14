3d8b13e73af0e1ef34cdafaf53fdffd2bb1b9b2e
antoine-rocher <antoine.rocher@epfl.ch>
2025-05-14 08:05:38 -0700

    add desi func

--- HODDIES/fits_functions.py
+++ HODDIES/fits_functions.py
@@ -1,6 +1,6 @@
 import numpy as np
 import os
-
+import glob
 
 def genereate_training_points(nPoints, priors, sampling_type='lhs', path_to_save_training_point=None, rand_seed=None):
     """
@@ -136,11 +136,129 @@ def multivariate_gelman_rubin(chains):
 
 
 
+def func_stochopy(new_params, HOD_obj, name_param, data, inv_cov2, fix_seed=10): 
+    new_params= np.array([new_params])
+    
+    new_params.dtype = [(name, dt) for name, dt in zip(name_param, ['float64']*len(name_param))]
+    HOD_obj.update_new_param(new_params, name_param)
+    cats = HOD_obj.make_mock_cat(fix_seed=fix_seed, verbose=False) 
+
+    result = {}
+    if 'wp' in HOD_obj.args['fit_param']["fit_type"]:
+        result['wp']= HOD_obj.get_crosswp(cats, tracers=HOD_obj.args['tracers'], verbose=False)
+    if 'xi' in HOD_obj.args['fit_param']["fit_type"]:
+        result['xi'] = HOD_obj.get_cross2PCF(cats, tracers=HOD_obj.args['tracers'], verbose=False)
+
+    stats = ['wp', 'xi'] if ('wp' in HOD_obj.args['fit_param']["fit_type"]) & ('xi' in HOD_obj.args['fit_param']["fit_type"]) else ['wp'] if ('wp' in HOD_obj.args['fit_param']["fit_type"]) else ['xi']
+    res = {}
+    comb_trs = result[stats[0]].keys() 
+    res = np.hstack([np.hstack([np.hstack(result[stat][comb_tr][1])for stat in stats]) for comb_tr in comb_trs])
+
+    chi2 = compute_chi2(res, data, inv_Cov2=inv_cov2)
+    print(new_params[0], chi2, flush=True)
+
+    return chi2
 
 
+import glob 
+from pycorr import TwoPointCorrelationFunction, utils
+import os
+import numpy as np 
 
 
+def get_corr_small_boxes(param, tracers, **kwargs):
+    
+    param.update(kwargs)
+    com_tr = np.vstack([np.array(np.meshgrid(tracers,tracers)).T.reshape(-1, len(tracers)).flatten().reshape(len(tracers),len(tracers),2)[i,i:] for i in range(len(tracers))])
+    str_tr_all = '_'.join(map(str, np.unique(com_tr)))
+    corr_fn = os.path.join(param['corr_dir'], f'z{param["z_simu"]:.3f}', f'Mcorr_small_box_z{param["z_simu"]}_{param["fit_type"]}_{str_tr_all}.txt')
+
+    if os.path.exists(corr_fn):
+        print(f'Load correlation matrix for {str_tr_all} at z{param["z_simu"]} ...', flush=True)
+        corr = np.loadtxt(corr_fn)
+        return corr
+    print('Load correlation matrix...', flush=True)
+    corr_type= ['rppi', 'smu'] if ('wp' in param['fit_type']) & ('xi' in param['fit_type']) else ['rppi'] if ('wp' in param['fit_type']) else ['smu']
+    res = []
+    for tr in com_tr:
+        tr = np.unique(tr)
+        str_tr = '_'.join(map(str, np.unique(tr)))
+        file_name = f'allcounts_{str_tr}_AbacusSummit_small_c000_ph*.npy'
+        file_dir = os.path.join(param['corr_dir'], f'z{param["z_simu"]:.3f}', '{}', file_name)
+
+        res_tr = []
+        for corr_t in corr_type:
+            if os.path.exists(os.path.join(file_dir.format(corr_t, str_tr))):
+                pass
+            else:
+                str_tr = '_'.join(map(str, np.unique(tr)[::-1]))
+            fns = glob.glob(os.path.join(file_dir.format(corr_t, str_tr)))
+            if len(fns) == 0:
+                raise FileNotFoundError(f'No {corr_t} measurements at z{param["z_simu"]} for {str_tr}...')
+            print(f'Load {corr_t} measurements at z{param["z_simu"]} for {str_tr}...', flush=True)
+            if corr_t == 'rppi':
+                res_tr += [[TwoPointCorrelationFunction.load(file)(pimax=param['pimax']) for file in fns]]
+            else:
+                res_tr += [[np.hstack(TwoPointCorrelationFunction.load(file)(ells=param['multipole_index'])) for file in fns]]
+        # print(len(glob.glob(f'/global/cfs/cdirs/desi/users/arocher/Y1/2PCF_for_corr/Abcaus_small_boxes/z{param["z_simu"]:.3f}/smu/allcounts_{param["z_simu"]}_AbacusSummit_small_c000_ph*.npy')))
+        # return 0
+        res += [np.hstack(res_tr)]        
+
+    corr = utils.cov_to_corrcoef(np.cov(np.hstack(res), rowvar=False, ddof=0))
+    np.savetxt(os.path.join(param['corr_dir'], f'z{param["z_simu"]:.3f}', f'Mcorr_small_box_z{param["z_simu"]}_{param["fit_type"]}_{str_tr_all}.txt'), corr)
+
+    return corr
 
 
+def load_desi_data(fit_param, tracers, **kwargs):
+    from pycorr import TwoPointCorrelationFunction
+    
+    print('Load data vector...', flush=True)
+    fit_param.update(kwargs)
+    if 'ELG' in tracers:
+        tracers[tracers.index('ELG')] = 'ELG_LOPnotqso'
+    
+    com_tr = np.vstack([np.array(np.meshgrid(tracers,tracers)).T.reshape(-1, len(tracers)).flatten().reshape(len(tracers),len(tracers),2)[i,i:] for i in range(len(tracers))])
+    
+    # data = {'wp': {}, 'xi': {}}  if ('wp' in fit_param['fit_type']) & ('xi' in fit_param['fit_type']) else {'wp': {}}  if ('wp' in fit_param['fit_type']) else {'xi': {}}
+    data = {}
+    data['edges'] = {}
+    stats= ['rppi', 'smu'] if ('wp' in fit_param['fit_type']) & ('xi' in fit_param['fit_type']) else ['rppi'] if ('wp' in fit_param['fit_type']) else ['smu']
+    if fit_param['load_cov_jk'] is not None:
+        cov_jk11 = []
+    for tr in com_tr:
+        str_tr = '_'.join(map(str, np.unique(tr)))
+        file_name = f"allcounts_{{}}_{fit_param['region']}_{fit_param['zmin']}_{fit_param['zmax']}_{fit_param['weights_type']}_{fit_param['bin_type']}_njack{fit_param['njack']}_nran{fit_param['nran']}_split20.npy"
+        file_dir = os.path.join(fit_param['dir_data'], '{}', file_name)
+
+        data[f'{tr[0][:3]}_{tr[1][:3]}'] = {}   
+        cov_jk1 = []
+        for corr in stats:
+            try:
+                result =  TwoPointCorrelationFunction.load(file_dir.format(corr, str_tr))
+            except FileNotFoundError as e:
+                str_tr = '_'.join(map(str, np.unique(tr)[::-1]))
+                result =  TwoPointCorrelationFunction.load(file_dir.format(corr, str_tr))
+            print(f'Load {corr} measurements at z{fit_param["zmin"]}-{fit_param["zmax"]} for {str_tr}...', flush=True)
+
+            if corr == 'rppi':
+                result= result[::2][4:-3]
+                data[f'{tr[0][:3]}_{tr[1][:3]}']['wp'] = result(return_sep=True, return_std=True, pimax=fit_param['pimax'])
+                data['edges']['wp'] = result.edges
+                if fit_param['load_cov_jk'] is not None:
+                    cov_jk1 += [[result.realization(i)(pimax=fit_param['pimax']) for i in range(result.nrealizations)]]
+
+            elif corr == 'smu':
+                result= result[16:-6]
+                data[f'{tr[0][:3]}_{tr[1][:3]}']['xi'] = result(return_sep=True, return_std=True, ells=fit_param['multipole_index'])
+                data['edges']['xi'] = result.edges
+                if fit_param['load_cov_jk'] is not None:
+                    cov_jk1 += [np.vstack([np.hstack(result.realization(i)(ells=fit_param['multipole_index'])) for i in range(result.nrealizations)])]
+        if fit_param['load_cov_jk'] is not None: cov_jk11 += [np.hstack(cov_jk1)]
+        
+    if fit_param['load_cov_jk'] is not None:
+        data['cov_jk'] = ((result.nrealizations-1) * np.cov(np.hstack(cov_jk11), rowvar=False, ddof=0))
+    tracers[tracers.index('ELG_LOPnotqso')] = 'ELG'
+    return data
 
 
