5f682db756bc57bb7cc4d6c5e599fbf9ecdea032
antoine-rocher <antoine.rocher@epfl.ch>
2025-05-16 03:01:20 -0700

    modifs

--- HODDIES/desi/run_mock_for_corr.sh
+++ HODDIES/desi/run_mock_for_corr.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=corrr_mocks
+#SBATCH --job-name=corr_mocks
 #SBATCH -p debug
 #SBATCH -C cpu
 #SBATCH -N 1
@@ -9,11 +9,11 @@
 
 source /global/common/software/desi/users/adematti/cosmodesi_environment.sh 2025_03
 
-for zsim in 1.850  1.550 1.250
+for zsim in 0.95
 do
     for ph in {3000..5000}  
     do
-        srun -n 1 -c 16 python /global/homes/a/arocher/Code/postdoc/HOD/Dev/HODDIES/HODDIES/desi/mock_for_corr_small_boxes.py --dir_param_file /global/homes/a/arocher/Code/postdoc/HOD/Dev/HODDIES/HODDIES/desi/parameters_QSO.yaml --phase $ph --zsim $zsim &
+        srun -n 1 -c 16 python /global/homes/a/arocher/Code/postdoc/HOD/Dev/HODDIES/HODDIES/desi/mock_for_corr_small_boxes.py --dir_param_file /global/homes/a/arocher/Code/postdoc/HOD/Dev/HODDIES/HODDIES/desi/test_fit_param_copy.yaml --phase $ph --zsim $zsim &
         
     done
 done
